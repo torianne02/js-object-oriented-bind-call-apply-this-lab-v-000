f81654bb0477cab9be7cdea201d849e62ff714b3
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 18:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 }
 
-function setThisWithApply(fn, thisValue, arg) {
-  return fn.apply(thisValue, arg)
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
 }
